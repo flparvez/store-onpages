82d46b7f1a3b2307345db3be0c27290ec0c56f89
flparvez <flparvez23@gmail.com>
2024-10-03 13:49:55 +0600

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 import {Provider,store} from "@/components/index"
-
+// test
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
